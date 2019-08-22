1867a771014b5ccd693de9d7c66577c36b520d51
yaozeye <yaozeye@outlook.com>
2019-08-22 22:08:55 +0800

    ver 1.0.1

--- douyu_com_night_mode.js
+++ douyu_com_night_mode.js
@@ -1,9 +1,13 @@
 // ==UserScript==
 // @name         douyu.com night mode
+// @name:zh-CN   斗鱼网页夜间模式
+// @name:zh-TW   斗魚網頁夜間模式
 // @namespace    https://greasyfork.org/users/282572
 // @homepageURL     https://github.com/yaozeye/javascript
-// @version      1.0
-// @description  DarkBack-BrightFont on douyu.com
+// @version      1.0.1
+// @description  Make background black on douyu.com
+// @description:zh-CN   网页端斗鱼(douyu.com)黑色背景
+// @description:zh-TW   網頁端斗魚(douyu.com)黑色背景
 // @author       yaozeye
 // @copyright   2019, yaozeye
 // @match        *://douyu.com/*
