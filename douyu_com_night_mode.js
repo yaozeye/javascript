78170e5c97c4ebee1f3d91f0c178272de97da96f
yaozeye <yaozeye@outlook.com>
2019-08-22 21:55:52 +0800

    Create douyu_com_night_mode.js

---
+++ douyu_com_night_mode.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         douyu.com night mode
+// @namespace    https://greasyfork.org/users/282572
+// @homepageURL     https://github.com/yaozeye/javascript
+// @version      1.0
+// @description  DarkBack-BrightFont on douyu.com
+// @author       yaozeye
+// @copyright   2019, yaozeye
+// @match        *://douyu.com/*
+// @match        *://*.douyu.com/*
+// @grant        none
+// @run-at       document-end
+// @license MIT https://opensource.org/licenses/MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.body.style.background="#000";
+    document.body.style.color="#fff";
+})();
